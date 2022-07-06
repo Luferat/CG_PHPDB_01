91412db2d06cb8a981fcdb39117e01ff49311a0b
André Luferat <andre@luferat.net>
2022-07-06 20:07:52 -0300

    Primeiros Ajustes

---
+++ _header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/img/favicon.png">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <title>Cripei</title>
+</head>
+
+<body>
+
+    <a id="top"></a>
+
+    <div class="wrap">
+
+        <header>
+            <a href="/" title="Página inicial"><img src="/img/logo01.128.png" alt="Logotipo de Cripei"></a>
+            <h1>Cripei<small>Cripe e resolva!</small></h1>
+        </header>
+
+        <nav>
+            <a href="/" title="Página inicial">
+                <i class="fa-solid fa-house fa-fw"></i>
+                <span>Início</span>
+            </a>
+            <a href="/contacts" title="Faça contato">
+                <i class="fa-solid fa-comments fa-fw"></i>
+                <span>Contatos</span>
+            </a>
+            <a href="/about" title="Sobre...">
+                <i class="fa-solid fa-circle-info fa-fw"></i>
+                <span>Sobre</span>
+            </a>
+            <a href="/user" title="Logue-se...">
+                <i class="fa-solid fa-user fa-fw"></i>
+                <span>Entrar</span>
+            </a>
+        </nav>
+
+        <main>
---
+++ _model.php
@@ -0,0 +1,24 @@
+
+<article>
+
+    <h2>Título</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi quia, debitis eaque repellat quae hic fuga explicabo quos temporibus rerum! Unde numquam possimus in sint commodi tenetur sequi placeat est!</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime, temporibus? Asperiores dolorem, in, facilis earum debitis nobis amet nostrum odit obcaecati velit consectetur soluta nemo enim saepe non laborum distinctio.</p>
+    <img src="https://picsum.photos/200/200" alt="Imagem aleatória">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea eum inventore doloremque deserunt beatae veritatis vitae soluta magnam provident totam perferendis debitis consequatur deleniti iste impedit porro delectus, quam animi?</p>
+    <h3>Links legais:</h3>
+    <ul>
+        <li><a href="https://github.com/Luferat">GitHub do Fessô</a></li>
+        <li><a href="https://youtube.com">Vídeo legais</a></li>
+        <li><a href="https://instagram.com">Instagram da galera</a></li>
+    </ul>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo asperiores placeat nostrum totam, a numquam ipsam reiciendis veniam vitae nihil amet quasi eum in quia, explicabo similique eos aut. Similique?</p>
+
+</article>
+
+<aside>
+
+    <h3>Barra lateral</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores placeat ullam blanditiis, inventore expedita earum non voluptate voluptatum.</p>
+
+</aside>
