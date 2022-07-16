7b9e238e7b42846986cfb637f563eace9c91c753
André Luferat <33375030+Luferat@users.noreply.github.com>
2022-07-15 21:36:49 -0300

    aula de hoje

--- _config.php
+++ _config.php
@@ -55,3 +55,38 @@ $conn->query('SET character_set_results=utf8');
 // Seta dias da semana e meses do MySQL/MariaDB para "português do Brasil":
 $conn->query('SET GLOBAL lc_time_names = pt_BR');
 $conn->query('SET lc_time_names = pt_BR');
+
+/************************
+ * Funções de uso geral *
+ ***********************/
+
+// Sanitiza campos de formulários usando method="POST":
+// Outros filtros podem ser implementados nesta função.
+function post_clean($post_field, $type = 'string')
+{
+
+    // Escolhe o tipo de filtro
+    switch ($type):
+        case 'string':
+
+            // Sanitiza strings
+            $post_value = htmlspecialchars($_POST[$post_field]);
+            break;
+
+        case 'email':
+
+            // Sanitiza endereços de e-mail
+            $post_value = filter_input(INPUT_POST, $post_field, FILTER_SANITIZE_EMAIL);
+            break;
+
+    endswitch;
+
+    // Remove excesso de espaços
+    $post_value = trim($post_value);
+
+    // Remove aspas perigosas
+    $post_value = stripslashes($post_value);
+
+    // Retorna valor do campo sanitizado
+    return $post_value;
+}
---
+++ contacts/index.php
@@ -0,0 +1,145 @@
+<?php
+
+/**
+ * Inclui o arquivo de configuração global do aplicativo:
+ */
+require($_SERVER['DOCUMENT_ROOT'] . '/_config.php');
+
+/**
+ * Define o título desta página:
+ **/
+$page_title = 'Faça Contato';
+
+/**
+ * Define o conteúdo principal desta página:
+ * 
+ * Esta viarável será exibida dentro da tag <article>...</article>.
+ */
+$page_article = '<h2>Faça Contato</h2>';
+
+/**
+ * Define o conteúdo da barra lateral desta página:
+ * 
+ * Esta variável será exibida na tag <aside>...</aside>.
+ */
+$page_aside = '';
+
+/***********************************************
+ * Todo o código PHP desta página começa aqui! *
+ ***********************************************/
+
+// Valores iniciais dos campos:
+$form = array(
+    'name' => 'Joca da Silva',
+    'email' => 'joca@silva',
+    'subject' => 'Contado do Joca',
+    'message' => 'Mensagem do Joca'
+);
+
+// Action do form:
+$action = htmlspecialchars($_SERVER["PHP_SELF"]);
+
+// Formulário de contatos:
+$html_form = <<<HTML
+
+<form action="{$action}" method="post" id="contact">
+
+    <p>
+        <label for="name">Nome:</label>
+        <input type="text" name="name" id="name" placeholder="Seu nome completo." required minlength="3" class="valid" value="{$form['name']}">
+    </p>
+
+    <p>
+        <label for="email">E-mail:</label>
+        <input type="email" name="email" id="email" placeholder="Exemplo: joca@silva.com" required class="valid" value="{$form['email']}">
+    </p>
+
+    <p>
+        <label for="subject">Assunto:</label>
+        <input type="text" name="subject" id="subject" placeholder="Assunto do contato." required minlength="3" class="valid" value="{$form['subject']}">
+    </p>
+
+    <p>
+        <label for="message">Mensagem:</label>
+        <textarea name="message" id="message" placeholder="Sua mensagem." required minlength="5" class="valid">{$form['message']}</textarea>
+    </p>
+
+    <p>
+        <button type="submit" class="primary">Enviar</button>
+    </p>
+
+</form>
+
+HTML;
+
+// Se o formulário foi enviado...
+if ($_SERVER["REQUEST_METHOD"] == "POST") :
+
+    /**
+     * Processa o formulário:
+     */
+
+    // Limpa (saminitiza) o valor do campos:
+    $name = post_clean('name', 'string');
+    $email = post_clean('email', 'email');
+    $subject = post_clean('subject', 'string');
+    $message = post_clean('message', 'string');
+
+    // Detecta campos vazios (ou, não aprovados na sanitização)
+    if ($name == '' or $email == '' or $subject == '' or $message == '') :
+
+        $page_article = <<<HTML
+
+        <h2>Faça Contato</h2>
+        <p>Preencha todos os campos abaixo para nos enviar uma mensagem.</p>
+   
+        <div class="feedback_error">
+            <strong>Olá!</strong>
+            <p>Um ou mais campos do formulário estão vazios!</p>
+            <p>Não foi possível enviar seu contato.</p>
+            <p>Por favor, preencha todos os campos e tente novamente.</p>
+        </div>
+
+        {$html_form}
+   
+HTML;
+
+    else :
+
+        echo 'Processando form';
+
+    endif;
+
+else :
+
+    // Exibe o formulário:
+    $page_article = $html_form;
+
+endif;
+
+
+/***********************************
+ * Fim do código PHP desta página! *
+ ***********************************/
+
+/**
+ * Inclui o cabeçalho do template nesta página:
+ */
+require($_SERVER['DOCUMENT_ROOT'] . '/_header.php');
+
+/**
+ * Exibe o conteúdo da página:
+ */
+
+echo <<<HTML
+
+<article>{$page_article}</article>
+
+<aside>{$page_aside}</aside>
+
+HTML;
+
+/**
+ * Inclui o rodapé do template nesta página.
+ */
+require($_SERVER['DOCUMENT_ROOT'] . '/_footer.php');
